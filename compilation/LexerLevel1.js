ebb93bc7b12f549cf90966a3bf8708cbc6859010
yinhe <yinhe@creditease.cn>
2020-02-11 23:24:17 +0800

    0.0.1版本

---
+++ compilation/LexerLevel1.js
@@ -0,0 +1,228 @@
+/**
+ * Created by yinhe on 2020/2/10.
+ */
+
+// token 含义type 和value 属性
+function Token (type, value) {
+    this.type = type;
+    this.value = value || '';
+}
+
+Utils = {
+    // 是 0~9
+    isNum(ch) {
+        return /^\d$/.test(ch)
+    },
+    // 是字母
+    isAlpha(ch) {
+        return /^([a-z]|[A-Z])$/.test(ch)
+    },
+    // 判断空格
+    isBlank(ch) {
+        return ch == ' ' || ch == '\t' || ch == '\n';
+    }
+}
+
+/**
+ * 解析简单的词法
+ *
+ * a > 35
+ * var a = 1
+ * a + b  * 3
+ *
+ */
+class LexerLevel1 {
+    constructor(code) {
+        // 定义状态机的状态
+        this.DfaState = {
+            Initial: "Initial",
+            If: "If", Id_if1:"Id_if1", Id_if2:"Id_if2",
+            Else:"Else", Id_else1:"Id_else1", Id_else2:"Id_else2", Id_else3:"Id_else3", Id_else4:"Id_else4",
+            Var:"Var", Id_var1:"Id_var1", Id_var2:"Id_var2", Id_var3:"Id_var3",
+            Identifier:"Identifier", GT:"GT", GE:"GE",
+            Assignment:"Assignment",
+            Plus:"Plus", Minus:"Minus", Star:"Star", Slash:"Slash",
+            SemiColon:"SemiColon",
+            LeftParen:"LeftParen",
+            RightParen:"RightParen",
+            Num:"Num"
+        }
+
+        this.tokens = []
+
+        this.code = code;
+
+        this.token = null;
+    }
+    
+    tokenRead() {
+        return this.tokens.length > 0 ? this.tokens.shift() : null
+    }
+    tokenPeek() {
+        return this.tokens[0]
+    }
+
+
+    /**
+     * 1. 将原token放入到tokens, 初始化token
+     * 2. 判断下一个状态
+     */
+    toInitial(ch) {
+
+        if(this.token && this.token.value.length > 0) {
+            this.tokens.push(this.token)
+        }
+        this.token = new Token();
+
+        let state = this.DfaState.Initial // 初始化状态
+
+        if(Utils.isAlpha(ch)) {
+            if(ch === 'v') {
+                state = this.DfaState.Id_var1
+            } else {
+                state = this.DfaState.Identifier
+            }
+            this.token.type = this.DfaState.Identifier
+            this.token.value = this.token.value + ch;
+        } else if(Utils.isNum(ch)) {
+            state = this.DfaState.Num
+            this.token.type = this.DfaState.Num
+            this.token.value = this.token.value + ch;
+        } else if (ch == ';') {
+            state = this.DfaState.SemiColon;
+            this.token.type = this.DfaState.SemiColon;
+            this.token.value = this.token.value + ch;
+        } else if (ch == '>') {
+            state = this.DfaState.GT;
+            this.token.type = this.DfaState.GT;
+            this.token.value = this.token.value + ch;
+        } else if (ch == '=') {
+            state = this.DfaState.Assignment;
+            this.token.type = this.DfaState.Assignment;
+            this.token.value = this.token.value + ch;
+        } else if (ch == '+') {
+            state = this.DfaState.Plus;
+            this.token.type = this.DfaState.Plus;
+            this.token.value = this.token.value + ch;
+        } else if (ch == '-') {
+            state = this.DfaState.Minus;
+            this.token.type = this.DfaState.Minus;
+            this.token.value = this.token.value + ch;
+        } else if (ch == '*') {
+            state = this.DfaState.Star;
+            this.token.type = this.DfaState.Star;
+            this.token.value = this.token.value + ch;
+        } else if (ch == '/') {
+            state = this.DfaState.Slash;
+            this.token.type = this.DfaState.Slash;
+            this.token.value = this.token.value + ch;
+        }
+
+        return state;
+    }
+
+    switchState(newState, val) {
+        this.token.type = newState
+        this.token.value = this.token.value + val;
+        return newState
+    }
+
+    /**
+     * 运行自动机,解析出token
+     */
+    dfaParse() {
+        let code = this.code
+        let codeLen = code.length;
+        let chIndex = 0;
+        let ch = ''
+        let state = this.DfaState.Initial // 初始化状态
+        while(chIndex < codeLen) {
+            ch = code.charAt(chIndex);
+            switch(state) {
+                case this.DfaState.Initial:
+                    state = this.toInitial(ch)
+                    break;
+                case this.DfaState.Id_var1:
+                    if(ch === 'a') {
+                        state = this.DfaState.Id_var2
+                        this.token.value = this.token.value + ch;
+                    } else if(Utils.isAlpha(ch) || Utils.isNum(ch)){
+                        state = this.switchState(this.DfaState.Identifier)
+                    } else {
+                        state = this.toInitial(ch)
+                    }
+                    break;
+                case this.DfaState.Id_var2:
+                    if(ch === 'r') {
+                        state = this.DfaState.Id_var2
+                        this.token.value = this.token.value + ch;
+                    } else if(Utils.isAlpha(ch) || Utils.isNum(ch)){
+                        state = this.switchState(this.DfaState.Identifier)
+                    } else {
+                        state = this.toInitial(ch)
+                    }
+                    break;
+                case this.DfaState.Id_var3:
+                    if(Utils.isAlpha(ch) || Utils.isNum(ch)){
+                        state = this.switchState(this.DfaState.Identifier)
+                    } else {
+                        state = this.toInitial(ch)
+                    }
+                    break;
+                case this.DfaState.Identifier:
+                    if(Utils.isAlpha(ch) || Utils.isNum(ch)){
+                        state = this.switchState(this.DfaState.Identifier)
+                    } else {
+                        state = this.toInitial(ch)
+                    }
+                    break;
+                case this.DfaState.Num:
+                    if(!Utils.isNum(ch)) {
+                        state = this.toInitial(ch)
+                    }
+                    break;
+                case this.DfaState.GT:
+                case this.DfaState.Assignment:
+                case this.DfaState.Plus:
+                case this.DfaState.Minus:
+                case this.DfaState.Star:
+                case this.DfaState.Slash:
+                case this.DfaState.SemiColon:
+                    state = this.toInitial(ch)
+                    break;
+                default:
+
+            }
+            chIndex++;
+
+        }
+        // 字符
+        state = this.toInitial(ch)
+
+    }
+
+
+}
+
+module.exports = LexerLevel1
+
+// test
+
+function main () {
+    let lexer = new LexerLevel1("var a = 1");
+
+    lexer.dfaParse();
+    console.log(lexer.tokens)
+
+    let lexer2 = new LexerLevel1("a > 2");
+
+    lexer2.dfaParse();
+    console.log(lexer2.tokens)
+
+    let lexer3 = new LexerLevel1("2 + 4 * 3");
+
+    lexer3.dfaParse();
+    console.log(lexer3.tokens)
+}
+
+// main()
---
+++ compilation/SyntaxLevel1.js
@@ -0,0 +1,183 @@
+/**
+ * Created by yinhe on 2020/2/11.
+ */
+
+const LexerLevel = require("./LexerLevel1")
+
+/**
+ * Ast 节点对象
+ * @param type
+ * @param value
+ * @constructor
+ */
+function AstNode (type, value) {
+    this.type = type;
+    this.value = value || '';
+    this.children = []
+    this.parent = null
+}
+
+AstNode.prototype.addChild = function(child) {
+    this.children.push(child)
+    child.parent = this;
+}
+
+/**
+ * 简单语法解析器
+ *
+ * 语法规则基于指定的文法, 所以需要先确定文法规则(详见对应的表达式处理方法)
+ *
+ *
+ * 由于语法解析存在优先级, 所以需要分级处理
+ * 1. 定义primary Expressions
+ * 2. 定义multiplicative Expression
+ * 3. 定义additive Expression
+ *
+ * 解析如下语法:
+ *   3 * 4 + 2
+ *   3 * 4
+ *   2 + 3 * 4
+ *
+ */
+class SyntaxLevel1 {
+    constructor(code) {
+        this.code = code;
+        // 词法分析对象
+        this.lexerLevel = new LexerLevel(code)
+        // 词法分析
+        this.lexerLevel.dfaParse();
+
+        this.tokens = this.lexerLevel.tokens;
+    }
+
+
+    /**
+     * 解析生成AST
+     */
+    astParse() {
+        // 先直接用加法表达式测算
+       return this.additive()
+    }
+
+    /**
+     * 加减语法
+     * Additive Operators
+     * 简化如下
+     * AdditiveExpression :
+     *      MultiplicativeExpression
+     *      AdditiveExpression + MultiplicativeExpression
+     *      AdditiveExpression - MultiplicativeExpression
+     *
+     */
+    additive() {
+        let tokens = this.tokens;
+        let child = this.multiplicative();
+        let node = child;
+
+        let nextToken = this.lexerLevel.tokenPeek()
+        if(child && nextToken) {
+            if(nextToken.type === this.lexerLevel.DfaState.Plus || nextToken.type === this.lexerLevel.DfaState.Minus) {
+                nextToken = this.lexerLevel.tokenRead();
+                // 判断右侧是否是一个乘法表达式
+                let childRight = this.multiplicative();
+                if(childRight) {
+                    // 构建一个加法表达式 AstNode
+                    node = new AstNode("Additive", nextToken.value);
+                    node.addChild(child)
+                    node.addChild(childRight)
+                } else {
+                    throw Error("error Additive Expression")
+                }
+            }
+        }
+        return node
+    }
+
+    /**
+     * 乘除语法
+     * Multiplicative Operators
+     * 简化乘法语法如下
+     * MultiplicativeExpression :
+     *      PrimaryExpression
+     *      MultiplicativeExpression * PrimaryExpression
+     *      MultiplicativeExpression / PrimaryExpression
+     *
+     * 
+     */
+    multiplicative() {
+        let tokens = this.tokens;
+        // 先判断是否符合基本表达式, 如果符合基本表达式, 暂时就已经找到了一个叶子节点(TOKEN)
+        let child = this.primary();
+        let node = child;
+
+        let nextToken = this.lexerLevel.tokenPeek()
+        if(child && nextToken) {
+            if(nextToken.type === this.lexerLevel.DfaState.Star || nextToken.type === this.lexerLevel.DfaState.Slash) {
+                nextToken = this.lexerLevel.tokenRead();
+                // 表示右侧必有一个字面量或者表达式
+                let childRight = this.primary();
+                if(childRight) {
+                    // 构建一个乘法表达式 AstNode
+                    node = new AstNode("Multiplicative", nextToken.value);
+                    node.addChild(child)
+                    node.addChild(childRight)
+                } else {
+                    throw Error("error Multiplicative Expression")
+                }
+            }
+        }
+
+        return node;
+
+    }
+
+
+    /**
+     * primary Expressions
+     * 针对最基本的预计, 我们设定文法规则如下, 只处理标识符 和 Number类型的字面量
+     * PrimaryExpression :
+     *      Identifier
+     *      Literal 
+     */
+    primary() {
+        let tokens = this.tokens;
+        let node = null;
+
+        // 预读判断
+        let nextToken = this.lexerLevel.tokenPeek()
+        if(nextToken) {
+            // 判断是否符合文法规则
+            if(nextToken.type === this.lexerLevel.DfaState.Identifier) {
+                nextToken = this.lexerLevel.tokenRead();
+                node = new AstNode(this.lexerLevel.DfaState.Identifier, nextToken.value);
+            } else if(nextToken.type === this.lexerLevel.DfaState.Num) {
+                nextToken = this.lexerLevel.tokenRead();
+                node = new AstNode(this.lexerLevel.DfaState.Num, nextToken.value);
+            }
+            // 其他默认先不处理
+        }
+
+        return node
+
+    }
+
+}
+
+
+function main () {
+
+   let syntaxLevel = new SyntaxLevel1("2+3*4")
+
+   console.log(syntaxLevel.astParse())
+
+    let syntaxLevel2 = new SyntaxLevel1("3*4+2")
+
+    console.log(syntaxLevel2.astParse())
+
+    let syntaxLevel3 = new SyntaxLevel1("3*4")
+
+    console.log(syntaxLevel3.astParse())
+
+}
+
+main()
